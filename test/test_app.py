02a34d2b0903c4d931c3395d73262b9e433f951c
Rohan Chakravarthy <rohanc219@gmail.com>
2015-11-01 19:37:05 -0500

    Coverage Reports+More tests

--- test/test_app.py
+++ test/test_app.py
@@ -1,4 +1,5 @@
 from source.app import get_resp_dict
+from source.app import app
 import pytest
 
 
@@ -6,8 +7,13 @@ def get_giphy_string():
     return "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="
 
 
-def test_good_response():
+def test_giphy_response():
     url = get_giphy_string()
     resp = get_resp_dict(url)
     if resp is None:
         pytest.fail("Unable to get data from giphy")
+
+
+def test_response_obj():
+    resp = app.test_client().get('/')
+    assert(resp.status_code == 200)
